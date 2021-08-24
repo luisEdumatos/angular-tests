55256a30637dc206ddfa2c5277de8e807b9cfa0d
luis.eduardo <luisedu.unifal@gmail.com>
2021-08-24 10:38:17 -0300

    Class conditional

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,24 @@
+import { Hero } from './../hero';
+import { HEROES } from './../mock-heroes';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSelect(hero) {
+    this.selectedHero = hero;
+  }
+
+}
